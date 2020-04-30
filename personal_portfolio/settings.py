0ee534f6bff2435298ffcdd3c10c50b08427e698
avisalmon <avi.salmon@intel.com>
2020-04-30 16:40:22 +0000

    changes in server

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gk-y3)armhqd&!5fk9^5p@vd1$(=%*u^21q(!enr2z2@!2%-s6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['avisalmon.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
